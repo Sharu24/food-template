1aafe869e63592d611a3c24daa351b4ba489ee28
Sharu24 <sharu.mukti@gmail.com>
2020-11-18 10:25:26 +0530

    updated JS module

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class FoodItem extends Component {
   render() {
     const { id, imageUrl, foodDesc, foodDetails } = this.props.food;
-    console.log(this.props);
+    console.log(id);
     return (
       <div className="w3-quarter">
         <img src={imageUrl} alt="Sandwich" style={{ width: "100%" }} />
